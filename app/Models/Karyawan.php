dd295fc4242ca3c56ae57220d628f3e14181d7df
hafifradhitya <bangclasher81@gmail.com>
2024-10-01 12:44:09 +0700

    update

---
+++ app/Models/Karyawan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Karyawan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'karyawan';
+
+    public function divisi()
+    {
+        return $this->belongsTo(Divisi::class, 'divisi_id', 'id');
+    }
+}
