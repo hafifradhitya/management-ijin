df217f767525b265fdd021a6a26082f10a18779f
hafifradhitya <bangclasher81@gmail.com>
2024-10-12 15:39:21 +0700

    first commit

---
+++ resources/js/pages/user.js
@@ -0,0 +1,23 @@
+import $ from 'jquery'
+import 'datatables.net'
+import 'datatables.net-bs5'
+import 'datatables.net-responsive'
+import 'datatables.net-responsive-bs5'
+
+window['$'] = $
+
+import 'datatables.net-bs5/css/dataTables.bootstrap5.min.css'
+import 'datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css'
+import { AjaxAction } from '../lib/utils'
+
+
+$('.main-content').on('click', '.action', function(e) {
+    const el = $(this)
+
+    if (!el.data('action')) {
+        throw new Error('Data attribute action must provide!!')
+    }
+
+    (new AjaxAction(el))
+    .execute()
+})
